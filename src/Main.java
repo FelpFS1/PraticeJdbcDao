c407424d1ebae1911d805cc7b843791050037718
FelpFS1 <phff800@gmail.com>
2025-02-28 20:53:42 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import db.DB;
+import db.DbException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/db/DB.java
@@ -0,0 +1,48 @@
+package db;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+    static Connection conn = null;
+
+    public static Connection getConnection(){
+
+        try{
+            Properties properties = loadProperties();
+            String url = properties.getProperty("dburl");
+            conn = DriverManager.getConnection(url,properties);
+            return conn;
+        }
+        catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
+
+    }
+
+    public static void closeConnection(){
+        if(conn != null){
+            try {
+            conn.close();
+
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    private static Properties loadProperties(){
+        try(FileInputStream stream = new FileInputStream("db.properties")){
+            Properties properties = new Properties();
+            properties.load(stream);
+
+            return properties;
+
+        } catch (Exception e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+}
