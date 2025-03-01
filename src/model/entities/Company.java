5ece118a3aa6e2b0be46cca8bf4862f470a6053e
FelpFS1 <phff800@gmail.com>
2025-02-28 21:19:50 -0300

    feat: implement Employee and Company entities

---
+++ src/model/entities/Company.java
@@ -0,0 +1,53 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Company implements Serializable {
+    private int id;
+    private String name;
+
+    public Company() {
+    }
+
+    public Company(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Company company = (Company) o;
+        return id == company.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/model/entities/Employee.java
@@ -0,0 +1,86 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Employee implements Serializable {
+    private int id;
+    private String name;
+    private int age;
+    private double salary;
+    private Company company;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int id, int age, Company company, double salary) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.company = company;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                ", company=" + company +
+                '}';
+    }
+}
